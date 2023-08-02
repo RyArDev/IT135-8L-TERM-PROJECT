5630b32a7392219d709ffc2a5e69097d31382354
denisemarcelo <denisenmarcelo@gmail.com>
2023-08-02 20:13:48 +0800

    draft

---
+++ components/services/services.js
@@ -0,0 +1,21 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // Get all elements with the 'download-link' class
+    const downloadLinks = document.querySelectorAll('.download-link');
+
+    // Attach click event listener to each download link
+    downloadLinks.forEach(link => {
+        link.addEventListener('click', function() {
+            // Get the filename from the data-filename attribute
+            const filename = this.getAttribute('data-filename');
+
+            // Update the PDF file URL with the correct path
+            const pdfUrl = '/assets/pdfs/' + filename;
+
+            // Create an anchor element and trigger the download
+            const anchor = document.createElement('a');
+            anchor.href = pdfUrl;
+            anchor.download = filename;
+            anchor.click();
+        });
+    });
+});
--- components/services/services.php
+++ components/services/services.php
@@ -1,24 +1,42 @@
 <?php 
-    //Imports CSS
+    // Imports CSS
     require('utilities/validation/server/css-validation.php');
     import_css("services");
     import_css("alert");
 
-    //Import Alert
+    // Import Alert
     include_once('components/alert/alert.php');
 
-    //Checks if the user is logged in before
+    // Checks if the user is logged in before
     include_once('utilities/authentication/auth-controller.php');
     $user = checkUserLogin();
     $_SESSION['current_page'] = "services";
+
+    // Array of forms and their corresponding PDF filenames
+    $forms = array(
+        "Form 1" => "Form1.pdf",
+        "Form 2" => "Form2.pdf",
+        "Form 3" => "Form3.pdf"
+        // Add more forms here as needed
+    );
 ?>
 
 <div>
-    This is the services page!
+    <h2>Forms:</h2>
+    <ul>
+        <?php foreach ($forms as $formName => $pdfFilename): ?>
+            <li>
+                <a class="download-link" href="/assets/pdfs/<?php echo $pdfFilename; ?>" download="<?php echo $pdfFilename; ?>">
+                    <?php echo $formName; ?>
+                </a>
+            </li>
+        <?php endforeach; ?>
+    </ul>
 </div>
 
+
 <?php 
-    //Imports Javascript
+    // Imports Javascript
     require_once('utilities/validation/server/js-validation.php');
     import_js("services");
     import_js("alert");
